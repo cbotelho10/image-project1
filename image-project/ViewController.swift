1783043944f1e6ace0a9d75039f044f2843f8b8c
chris botelho <chris.botelho29@gmail.com>
2021-09-23 14:11:08 -0400

    image project

--- image-project/ViewController.swift
+++ image-project/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var catImage: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func displayButton(_ sender: UIButton) {
+        if messageLabel.text == "where r my cats???" {
+            messageLabel.text = "hello there"
+            catImage.image = UIImage (named: "Cat")
+        } else{
+            if messageLabel.text == "cats r great" {
+                messageLabel.text = "here's another cat :)"
+                catImage.image = UIImage (named: "Cat2")
+            }
+        }
+    }
+    
 
 }
 
